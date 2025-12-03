5cc02f1fdeea8b1085b488adfae590768f25bbc4
namanbansalbusiness-gif <namanbansalbusiness@gmail.com>
2025-12-03 10:40:54 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   project2.c
    	modified:   project2.exe

--- project2.c
+++ project2.c
@@ -847,8 +847,48 @@ int main(){
 //     return 0;
 // }
 
-#include<stdio.h>
-int main(){
-    
+// WAP to print the 3 by 3 matrix i n spiral form
+
+#include <stdio.h>
+
+int main() {
+    int n = 3;
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int top = 0, bottom = n - 1;
+    int left = 0, right = n - 1;
+
+    printf("Spiral form of the matrix:\n");
+
+    while (top <= bottom && left <= right) {
+        // Print top row
+        for (int i = left; i <= right; i++)
+            printf("%d ", matrix[top][i]);
+        top++;
+
+        // Print right column
+        for (int i = top; i <= bottom; i++)
+            printf("%d ", matrix[i][right]);
+        right--;
+
+        // Print bottom row (if still valid)
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                printf("%d ", matrix[bottom][i]);
+            bottom--;
+        }
+
+        // Print left column (if still valid)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                printf("%d ", matrix[i][left]);
+            left++;
+        }
+    }
+
     return 0;
 }
