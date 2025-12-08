6171b38e3de743e502f988291d67b23c5e0c8489
namanbansalbusiness-gif <namanbansalbusiness@gmail.com>
2025-12-09 02:14:09 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   project2.c
    	modified:   project2.exe

--- project2.c
+++ project2.c
@@ -3,30 +3,37 @@
 #include <stdlib.h>
 
 int main() {
-    int opt = 0;          // initialize option
+    int opt;
     float num1, num2, res;
     int n, i;
-    long long fact;       // using long long for bigger numbers
+    long long fact;
     float val;
 
-    // Run the calculator loop until user chooses Exit (13)
-    while (opt != 13) {
-        printf("\n--- Scientific Calculator ---\n");
+    // Run the calculator loop
+    while (1) {
+        printf("\n");
+        printf("------- Scientific Calculator -------\n");
         printf("1. Addition\n");
         printf("2. Subtraction\n");
         printf("3. Multiplication\n");
         printf("4. Division\n");
         printf("5. Power (num ^ pow)\n");
         printf("6. Square Root\n");
-        printf("7. Natural Log (ln)\n");
+        printf("7. Log (natural)\n");
         printf("8. Log10 (base 10)\n");
-        printf("9. Sine\n");
-        printf("10. Cosine\n");
-        printf("11. Tangent\n");
+        printf("9. Sin\n");
+        printf("10. Cos\n");
+        printf("11. Tan\n");
         printf("12. Factorial\n");
         printf("13. Exit\n");
+        printf("-------------------------------------\n");
         printf("Select an option: ");
         scanf("%d", &opt);
+
+        if (opt == 13) {
+            printf("Exiting... Bye!\n");
+            break;
+        }
 
         switch (opt) {
             case 1:
@@ -92,7 +99,7 @@ int main() {
             case 7:
                 printf("Enter value: ");
                 scanf("%f", &num1);
-                if (num1 > 0)
+                if(num1 > 0)
                     printf("Result: %.4f\n", log(num1));
                 else
                     printf("Error: Log requires positive number.\n");
@@ -101,7 +108,7 @@ int main() {
             case 8:
                 printf("Enter value: ");
                 scanf("%f", &num1);
-                if (num1 > 0)
+                if(num1 > 0)
                     printf("Result: %.4f\n", log10(num1));
                 else
                     printf("Error: Log requires positive number.\n");
@@ -110,21 +117,22 @@ int main() {
             case 9:
                 printf("Enter angle in degrees: ");
                 scanf("%f", &num1);
-                val = num1 * M_PI / 180.0;   // use M_PI for precision
+                // convert to radians manually: deg * 3.14 / 180
+                val = num1 * 3.14159 / 180;
                 printf("Sin(%.1f) = %.4f\n", num1, sin(val));
                 break;
 
             case 10:
                 printf("Enter angle in degrees: ");
                 scanf("%f", &num1);
-                val = num1 * M_PI / 180.0;
+                val = num1 * 3.14159 / 180;
                 printf("Cos(%.1f) = %.4f\n", num1, cos(val));
                 break;
 
             case 11:
                 printf("Enter angle in degrees: ");
                 scanf("%f", &num1);
-                val = num1 * M_PI / 180.0;
+                val = num1 * 3.14159 / 180;
                 printf("Tan(%.1f) = %.4f\n", num1, tan(val));
                 break;
 
@@ -140,10 +148,6 @@ int main() {
                     }
                     printf("Factorial of %d is %lld\n", n, fact);
                 }
-                break;
-
-            case 13:
-                printf("Exiting... Bye!\n");
                 break;
 
             default:
